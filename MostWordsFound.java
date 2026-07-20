7b39828e602a4790e0eae9097bf493e34b10f93f
venkatesh-2008 <venkat20102008@gmail.com>
2026-07-20 11:08:30 +0530

    Add files via upload

---
+++ MostWordsFound.java
@@ -0,0 +1,9 @@
+public class MostWordsFound {
+    public int mostWordsFound(String[] sentences) {
+        int maxWords = 0;
+        for (String sentence : sentences) {
+            maxWords = Math.max(maxWords, sentence.split(" ").length);
+        }
+        return maxWords;
+    }
+}
