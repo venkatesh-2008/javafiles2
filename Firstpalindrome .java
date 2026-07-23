6d2d2d9110328575906efb5198893ce580da49dc
venkatesh-2008 <venkat20102008@gmail.com>
2026-07-23 13:30:42 +0530

    Add files via upload

---
+++ Firstpalindrome .java
@@ -0,0 +1,25 @@
+public class Firstpalindrome {
+    public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+
+    private boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
